a10d1172650f53588d466a1cfb0d880d7d576ddf
Yaakoub tair <yaakoubt05@gmail.com>
2025-09-08 16:53:53 +0200

    challenge 2

---
+++ challenge_2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+void multiplication(int a,int b){
+   int multip = a * b;
+  printf("la multiplication de %d * %d est: %d\n",a,b, multip);
+}
+int main(){
+    multiplication(5,4);
+
+
+
+
+
+
+
+
+
+    return 0;
+}
