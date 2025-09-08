838dea65d531e7c3edcd179832a9fe8518fbea1a
Yaakoub tair <yaakoubt05@gmail.com>
2025-09-08 16:46:49 +0200

    first commit

---
+++ challenge_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void somme(int a,int b){
+   int sum = a + b;
+  printf("la somme de %d + %d est: %d\n",a,b, sum);
+}
+int main(){
+    somme(5,8);
+
+
+
+
+
+
+
+
+    return 0;
+}
